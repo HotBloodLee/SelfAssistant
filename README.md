1eda384222e11b09a38622dfc1de5f23d0a7228e
davonli <1098422718@qq.com>
2025-06-26 00:34:56 +0800

    add config py

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ TODO:
 
 - [x] 添加Qwen调用支持
 - [x] SFT单批次测试
-- [ ] DPO单批次测试
+- [x] DPO单批次测试
+- [ ] config文件支持
+- [ ] pkg化
---
+++ core/utils/common_utils.py
@@ -0,0 +1,7 @@
+import yaml
+
+def load_config(config_path):
+    """Load config from yaml path."""
+    with open(config_path, "r") as fp:
+        config = yaml.safe_load(fp)
+    return config
